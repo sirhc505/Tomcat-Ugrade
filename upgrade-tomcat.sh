5b4c4f746627595ac1c34aa3f9c5dc1e3cd07ce2
Chris Tarricone <chris@uconn.edu>
2019-10-28 09:25:54 -0400

    Tomcat Upgrade Script

--- upgrade-tomcat.sh
+++ upgrade-tomcat.sh
@@ -19,18 +19,21 @@
 
 TOMCAT_VERSION_FILE="/opt/current_tomcat"
 TOMCAT_CURRENT=`cat $TOMCAT_VERSION_FILE`
-TOMCAT_LATEST=`curl -s http://mirrors.ibiblio.org/apache/tomcat/tomcat-9/ |grep v9 | cut -f 3 -d \> | cut -f 1 -d \/`
+TOMCAT_LATEST=`curl -s https://www.apache.org/dist/tomcat/tomcat-9/ |grep v9 | cut -f 3 -d \> | cut -f 1 -d \/`
 SHA_FILE=
 TOMCAT_VERSION=`echo $TOMCAT_LATEST | cut -f 2 -d v`
 TOMCAT_WORKING_DIR="/opt/tomcat9"
 TMP_TOMCAT="/tmp/apache-tomcat-$TOMCAT_VERSION.tar.gz"
 TMP_SHASUM="/tmp/apache-tomcat-$TOMCAT_VERSION.tar.gz.sha512"
 
+TOMCAT_BIN="https://www.apache.org/dist/tomcat/tomcat-9/$TOMCAT_LATEST/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz"
+TOMCAT_SHA="https://www.apache.org/dist/tomcat/tomcat-9/$TOMCAT_LATEST/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz.sha512"
+
 if [ "$TOMCAT_CURRENT" -ne "$TOMCAT_LATEST" ];	
 	echo "Our Tomcat: $TOMCAT_CURRENT, Latest Release: $TOMCAT_LATEST"
 	echo "Downloading Tomcat: $TOMCAT_LATEST..."
-	curl http://apache.mirrors.hoobly.com/tomcat/tomcat-9/$TOMCAT_LATEST/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz -o $TMP_TOMCAT
-	curl https://www.apache.org/dist/tomcat/tomcat-9/$TOMCAT_LATEST/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz.sha512 -o $TMP_SHASUM
+	curl $TOMCAT_BIN -o $TMP_TOMCAT
+	curl $TOMCAT_SHA -o $TMP_SHASUM
 	SHA_SUM = `sha512sum $TMP_TOMCAT`
 	ACTUAL_SUM = `cat $TMP_SHASUM`
 
@@ -43,11 +46,13 @@ if [ "$TOMCAT_CURRENT" -ne "$TOMCAT_LATEST" ];
 	echo "Stopping CAS..."
 	systemctl stop tomcat9
 	echo "Done!"
+
 	echo "Starting Upgrade Process..."
 	tar -zxf $TMP_TOMCAT -C $TOMCAT_WORKING_DIR --strip-components 1
 	chown tomcat.tomcat $TOMCAT_WORKING_DIR
 	chown tomcat.tomcat $TOMCAT_WORKING_DIR/* -R
 	echo "Upgrade Complete!"
+
 	echo "Restarting CAS..."
 	systemctl start tomcat9
 	echo "Done!"
