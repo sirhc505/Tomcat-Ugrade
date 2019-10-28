fc0c2bae2e7ddc62569162a1c01efbbff3363b0f
Chris Tarricone <chris@uconn.edu>
2019-10-28 11:17:26 -0400

    Additional validation

--- upgrade-tomcat.sh
+++ upgrade-tomcat.sh
@@ -17,10 +17,15 @@
 #	https://www.gnu.org/licenses/gpl-3.0.md
 ####################################################################################################################
 
-TOMCAT_VERSION_FILE="/opt/current_tomcat"
-TOMCAT_CURRENT=`cat $TOMCAT_VERSION_FILE`
+HOSTNAME=`hostname -f`
+MAIL_SERVER="smtp.uconn.edu"
+SERVER_ADMIN="chris@uconn.edu"
+SENDMAIL_UPGRADE_FAILED="/etc/ssmtp/upgrade_failed.txt"
+SENDMAIL_CHECKSUM_FAILED="/etc/ssmtp/checksum_failure.txt"
+
+TOMCAT_CURRENT=`java -classpath /opt/tomcat9/lib/catalina.jar org.apache.catalina.util.ServerInfo |grep "Server version" |cut -f 2 -d \/`
 TOMCAT_LATEST=`curl -s https://www.apache.org/dist/tomcat/tomcat-9/ |grep v9 | cut -f 3 -d \> | cut -f 1 -d \/`
-SHA_FILE=
+
 TOMCAT_VERSION=`echo $TOMCAT_LATEST | cut -f 2 -d v`
 TOMCAT_WORKING_DIR="/opt/tomcat9"
 TMP_TOMCAT="/tmp/apache-tomcat-$TOMCAT_VERSION.tar.gz"
@@ -29,7 +34,21 @@ TMP_SHASUM="/tmp/apache-tomcat-$TOMCAT_VERSION.tar.gz.sha512"
 TOMCAT_BIN="https://www.apache.org/dist/tomcat/tomcat-9/$TOMCAT_LATEST/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz"
 TOMCAT_SHA="https://www.apache.org/dist/tomcat/tomcat-9/$TOMCAT_LATEST/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz.sha512"
 
-if [ "$TOMCAT_CURRENT" -ne "$TOMCAT_LATEST" ];	
+# We need to send notifications if we have a failure of some kind.
+# If for some reason ssmtp is not installed we are going to do that
+# now and setup the error messages.
+if [ ! -f "/etc/ssmtp/ssmtp.conf"]; then
+	yum -y install ssmtp
+	sed -i s/root=postmaster/root=$SERVER_ADMIN/g /etc/ssmtp/ssmtp.conf
+	sed -i s/mailhub=mail/mailhub=$MAIL_SERVER/g /etc/ssmtp/ssmtp.conf
+	sed -i s/\#Hostname=/Hostname=$HOSTNAME/g /etc/ssmtp/ssmtp.conf
+
+	echo -e "Subject: Tomcat Upgrade Failed\nThe system was unable to upgrade Tomcat properly. Please login to $HOSTNAME check the system.\n" > $SENDMAIL_UPGRADE_FAILED
+	echo -e "Subject: Tomcat SHA Checksum failure\nThe system was unable to upgrade Tomcat properly. There was a mismatch of SHA512 sums from the downloaded file and expected SHA sum. Please login to $HOSTNAME check the system.\n" > $SENDMAIL_CHECKSUM_FAILED
+fi
+
+
+if [ "$TOMCAT_CURRENT" -ne "$TOMCAT_LATEST" ]; then
 	echo "Our Tomcat: $TOMCAT_CURRENT, Latest Release: $TOMCAT_LATEST"
 	echo "Downloading Tomcat: $TOMCAT_LATEST..."
 	curl $TOMCAT_BIN -o $TMP_TOMCAT
@@ -39,6 +58,7 @@ if [ "$TOMCAT_CURRENT" -ne "$TOMCAT_LATEST" ];
 
 	if [ "$SHA_SUM" -ne "$ACTUAL_SUM"];
 		echo "SHA sums do not match. Exiting..."
+		/usr/sbin/sendmail $SERVER_ADMIN < $SENDMAIL_CHECKSUM_FAILED
 		exit
 	fi
 
@@ -52,6 +72,14 @@ if [ "$TOMCAT_CURRENT" -ne "$TOMCAT_LATEST" ];
 	chown tomcat.tomcat $TOMCAT_WORKING_DIR
 	chown tomcat.tomcat $TOMCAT_WORKING_DIR/* -R
 	echo "Upgrade Complete!"
+
+	INSTALLED_VERSION=`java -classpath /opt/tomcat9/lib/catalina.jar org.apache.catalina.util.ServerInfo |grep "Server version" |cut -f 2 -d \/`
+
+	if [ "$INSTALLED_VERSION" -ne "$TOMCAT_VERSION"]; then
+		echo "Upgrade version mismatch. Something went wrong."
+		/usr/sbin/sendmail $SERVER_ADMIN < $SENDMAIL_UPGRADE_FAILED
+		exit
+	fi
 
 	echo "Restarting CAS..."
 	systemctl start tomcat9
