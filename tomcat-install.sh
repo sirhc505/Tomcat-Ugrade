1e8bdc9fe95139ba2380e459929b634893eeba39
Chris Tarricone <chris@uconn.edu>
2020-03-09 08:15:11 -0400

    Added quick install

--- tomcat-install.sh
+++ tomcat-install.sh
@@ -40,24 +40,41 @@ OPENSSL_CONFIG="$OPENSSL_CERT_DIR/openssl.cnf"
 CERT_REQ="$OPENSSL_CERT_DIR/server.csr"
 CERT_KEY="$OPENSSL_CERT_DIR/server.key"
 
+echo "--------------------------------------------------"
+echo "Installing software prereqs"
+echo "--------------------------------------------------"
 yum -y install httpd apr apr-devel httpd-devel libcurl-devel pcre-devel openssl-devel pam-devel java-11-openjdk mod_ssl
 
-mkdir $TOMCAT_WORKING_DIR
-
-$USERADD -d $TOMCAT_WORKING_DIR -c 'Tomcat Service Account' $TOMCAT_USER
-
 # Download Tomcat
+echo "--------------------------------------------------"
+echo "Downloading Tomcat"
+echo "--------------------------------------------------"
 $CURL $TOMCAT_BIN -o $TMP_TOMCAT
 $CURL $TOMCAT_SHA -o $TMP_SHASUM
 
 SHA_SUM=`sha512sum $TMP_TOMCAT`
 ACTUAL_SUM=`cat $TMP_SHASUM`
 
+echo "--------------------------------------------------"
+echo "Validating SHA sums for the Tomcat download"
+echo "--------------------------------------------------"
+if [[ "$TMP_TOMCAT" != "$TMP_SHASUM" ]]; then
+    echo "SHA sums do not match. Exiting..."
+    exit 1
+fi
+
+echo "--------------------------------------------------"
+echo "Creating Tomcat user account and directory"
+echo "--------------------------------------------------"
+mkdir $TOMCAT_WORKING_DIR
+$USERADD -d $TOMCAT_WORKING_DIR -c 'Tomcat Service Account' $TOMCAT_USER
+
 tar -zxf $TMP_TOMCAT -C $TOMCAT_WORKING_DIR --strip-components 1
-chown tomcat.tomcat $TOMCAT_WORKING_DIR
-chown tomcat.tomcat $TOMCAT_WORKING_DIR/* -R
 
 # Make needed Directories
+echo "--------------------------------------------------"
+echo "Creating Red Hat consisted Tomcat directories"
+echo "--------------------------------------------------"
 mkdir /etc/tomcat/
 mkdir -p /etc/tomcat/Catalina/localhost/
 mkdir /usr/libexec/tomcat/
@@ -66,9 +83,11 @@ mkdir /usr/share/tomcat
 mkdir -p /var/log/tomcat
 mkdir -p /var/lib/tomcat/webapps
 mkdir -p /var/cache/tomcat/work
-mkdir -p /etc/pki/shib/
 
 # Create symlinks to simulate an RPM based installation
+echo "--------------------------------------------------"
+echo "Linking all directories together"
+echo "--------------------------------------------------"
 ln -s /opt/tomcat9/lib /usr/share/tomcat/lib
 ln -s /opt/tomcat9/bin /usr/share/tomcat/bin
 ln -s /etc/tomcat /usr/share/tomcat/conf
@@ -78,18 +97,27 @@ ln -s /var/cache/tomcat/work /usr/share/tomcat/work
 ln -s /opt/shibboleth-idp/war/idp.war /var/lib/tomcat/webapps/idp.war
 
 # Copy configuration files
+echo "--------------------------------------------------"
+echo "Copying base Tomcat config"
+echo "--------------------------------------------------"
 cp configs/tomcat.conf /etc/tomcat/
 cp /opt/tomcat9/conf/* /etc/tomcat/
 cp libexec/* /usr/libexec/tomcat/
 
-# Localize the configuration file
-sed -i s/ServerAlias\ shib3.uits.uconn.edu/ServerAlias\ $LONG_HOST/g /etc/httpd/conf.d/shibboleth.uconn.edu.conf
-
 # Set folder ownership
+echo "--------------------------------------------------"
+echo "Setting Permissions"
+echo "--------------------------------------------------"
+chown tomcat.tomcat $TOMCAT_WORKING_DIR
+chown tomcat.tomcat $TOMCAT_WORKING_DIR/* -R
 chown tomcat.tomcat -R /etc/tomcat/*
 chown root.tomcat /var/lib/tomcat /var/lib/tomcats
 chown tomcat.tomcat /var/log/tomcat
 chown tomcat.tomcat /var/lib/tomcat /var/lib/tomcat/* /var/cache/tomcat /var/cache/tomcat/* /etc/tomcat /etc/tomcat/*
+
+echo "--------------------------------------------------"
+echo "Creating systemd startup script for Tomcat"
+echo "--------------------------------------------------"
 
 echo "[Unit]
 Description=Tomcat 9 servlet container
@@ -117,6 +145,9 @@ WantedBy=multi-user.target" > /etc/systemd/system/tomcat.service
 
 systemctl daemon-reload
 
+echo "--------------------------------------------------"
+echo "Creating Digital CSR for $LONG_HOST"
+echo "--------------------------------------------------"
 mkdir $OPENSSL_CERT_DIR
 
 echo "[req]
@@ -142,48 +173,36 @@ subjectAltName = @alt_names
 
 [alt_names]
 DNS.1 = $LONG_HOST
-DNS.1 = $SHORT_HOST.its.uconn.edu
-DNS.2 = shibboleth.uconn.edu
-DNS.3 = dev.shibboleth.uconn.edu" > $OPENSSL_CONFIG
+DNS.2 = $SHORT_HOST.its.uconn.edu
+DNS.4 = $SHORT_HOST.grove.ad.uconn.edu" > $OPENSSL_CONFIG
 
 $OPENSSL req -new -config $OPENSSL_CONFIG -keyout $CERT_KEY -out $CERT_REQ -nodes
+echo "--------------------------------------------------"
+echo "Go get yourself some certificates"
+echo "--------------------------------------------------"
+
+
 
 # Update Timezone & Time Servers
+echo "--------------------------------------------------"
+echo "Configuring timezone and setting NTP Servers"
+echo "--------------------------------------------------"
 timedatectl set-timezone America/New_York
 sed -i '/pool\ 2.rhel.pool.ntp.org\ iburst/i server\ time.uconn.edu\nserver\ time2.uconn.edu' /etc/chrony.conf
 sed -i s/pool\ 2.rhel.pool.ntp.org\ iburst//g /etc/chrony.conf
 systemctl restart chronyd.service
 
-echo "Go get yourself some certificates"
-
-echo "
-################################################################################
-# Once you have a real certificate, save it as server.crt                      #
-# Create a file incommon.crt which is the intermediate/root certs for InCommon #
-# Then:                                                                        #
-# cp server.crt server.key incommon.crt /etc/pki/shib/                         #
-# systemctl start httpd.service                                                #
-# systemctl start tomcat.service                                               #
-################################################################################
-"
-
-# Create redirect page
-echo "<!DOCTYPE html>
-<html>
-   <head>
-      <title>Shibboleth Redirect</title>
-      <meta http-equiv=\"refresh\" content=\"0;url=/idp/\" />
-   </head>
-   <body>
-      <p>Redirecting... If redirect does not happen <a href=\"/idp/\">click here</a></p>
-   </body>
-</html>" > /var/www/html/index.html
 
 # Enable services at book time
-systemctl enable httpd.service
+echo "--------------------------------------------------"
+echo "Setting Tomcat to start at Boot Time"
+echo "--------------------------------------------------"
 systemctl enable tomcat.service
 
 # Setup the firewall
+echo "--------------------------------------------------"
+echo "Configuring Bas Firewall for HTTP & HTTPS"
+echo "--------------------------------------------------"
 firewall-cmd --permanent --add-service=http
 firewall-cmd --permanent --add-service=https
 # Cockpit? Really? Because who wouldn't want a management gui on a server for pwning?
